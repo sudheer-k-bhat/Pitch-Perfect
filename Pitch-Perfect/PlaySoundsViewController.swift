1f4f4f24b3de9b72da00d6e8a85d42a09dc3180a
Sudheer Keshav Bhat <sudheer.k.bhat@gmail.com>
2015-12-27 19:58:08 +0530

    chipmunk & darthwader effects

--- Pitch-Perfect/PlaySoundsViewController.swift
+++ Pitch-Perfect/PlaySoundsViewController.swift
@@ -14,11 +14,17 @@ class PlaySoundsViewController: UIViewController {
     var player:AVAudioPlayer!
     
     var receivedAudio:RecordedAudio!
+    
+    var audioEngine:AVAudioEngine!
+    
+    var audioFile:AVAudioFile!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         player = try? AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
         player.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = try? AVAudioFile(forReading: receivedAudio.filePathUrl)
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,6 +42,35 @@ class PlaySoundsViewController: UIViewController {
     
     @IBAction func stopPlaying(sender: UIButton) {
         player.stop()
+    }
+    
+    @IBAction func playDarthvaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch:Float){
+        player.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
     }
     
     func play(rate:Float){
